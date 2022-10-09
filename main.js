14375beb73b24c779b046584fd412c44aba1363d
nathan wentworth <nwentworth95@gmail.com>
2022-10-09 14:48:10 -0400

    functioning!

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
 const config = require('./config.json');
-const auth = require('./auth.js');
+const auth = require('./auth.json');
 const loginToken = auth.token;
-const Discord = require(`discord.js`);
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require(`discord.js`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
 
 client.on(`ready`, () => {
   console.log('vtube bot started');
+
 });
 
-client.on(`message`, (message) => {
+client.on(`messageCreate`, (message) => {
   console.log('message');
 
   if (message.author.bot) return;
 
-  let twitterUrls = message.content.match(/\b(https?:\/\/.*?\.[a-z]{2,4}\/[^\s]*\b)/g);
-  if (twitterUrls.length > 0) {
+  let twitterUrls = message.content.match(/\b(https?:\/\/(mobile.)?twitter\.com\/[^\s]*\b)/g);
+  if (twitterUrls && twitterUrls.length > 0) {
     message.suppressEmbeds(true);
     let newUrls = [];
     for (var i = 0; i < twitterUrls.length; i++) {
       let url = twitterUrls[i];
-      newUrl = url.replace('/twitter', `/${ config.prefix }twitter`);
+      let newUrl = '';
+      newUrl = url.replace('mobile.', '');
+      newUrl = newUrl.replace(/\?.*\b/g, '');
+      newUrl = newUrl.replace('/twitter', `/${ config.prefix }twitter`);
       newUrls.push(newUrl);
     }
     message.channel.send({
-      content: newUrls.join('\n');
+      content: newUrls.join('\n')
     })
   }
 
 })
+
+client.login(loginToken);
