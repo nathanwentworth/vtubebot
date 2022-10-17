18ea47de61c0b4634b1fdc27fb12abc8c06d5b01
nathan wentworth <nwentworth95@gmail.com>
2022-10-17 17:13:39 -0400

    added spoiler and embed suppresion support

--- main.js
+++ main.js
@@ -15,28 +15,80 @@ client.on(`ready`, () => {
 
 });
 
+let newUrls = [];
+let messageContent = '';
+
 client.on(`messageCreate`, (message) => {
-  console.log('message');
 
   if (message.author.bot) return;
+  let hasEmbeds = messageHasEmbed(message.embeds);
+  
+  console.log('message, has embeds?', hasEmbeds, JSON.stringify(message.embeds));
 
-  let twitterUrls = message.content.match(/\b(https?:\/\/(mobile.)?twitter\.com\/[^\s]*\b)/g);
-  if (twitterUrls && twitterUrls.length > 0) {
-    message.suppressEmbeds(true);
-    let newUrls = [];
-    for (var i = 0; i < twitterUrls.length; i++) {
-      let url = twitterUrls[i];
-      let newUrl = '';
-      newUrl = url.replace('mobile.', '');
-      newUrl = newUrl.replace(/\?.*\b/g, '');
-      newUrl = newUrl.replace('/twitter', `/${ config.prefix }twitter`);
-      newUrls.push(newUrl);
+  // if (message.embeds.length > 0 && !hasEmbeds) {
+  //   return;
+  // }
+
+
+  newUrls = [];
+  messageContent = message.content;
+
+  let hiddenEmbeds = messageContent.match(/\<\b(https?:\/\/(mobile.)?twitter\.com\/[^\s]*\b)\>/g);
+  if (hiddenEmbeds && hiddenEmbeds.length > 0) {
+    for (var i = 0; i < hiddenEmbeds.length; i++) {
+      messageContent = messageContent.replace(hiddenEmbeds[i], '');
     }
-    message.channel.send({
-      content: newUrls.join('\n')
-    })
+    
   }
 
+  let spoiledTwitterUrls = messageContent.match(/\|\| *\b(https?:\/\/(mobile.)?twitter\.com\/[^\s]*\b) *\|\|/g);
+  if (spoiledTwitterUrls && spoiledTwitterUrls.length > 0) {
+    message.suppressEmbeds(true);
+    processUrls(spoiledTwitterUrls);
+  }
+
+  let twitterUrls = messageContent.match(/\b(https?:\/\/(mobile.)?twitter\.com\/[^\s]*\b)/g);
+  if (twitterUrls && twitterUrls.length > 0) {
+    console.log('embeds', JSON.stringify(message.embeds));
+    message.suppressEmbeds(true);
+    
+    processUrls(twitterUrls);
+  }
+
+  if (newUrls.length > 0) {
+    message.channel.send({
+      content: newUrls.join('\n')
+    });
+  }
+
+
+})
+
+function processUrls(urls) {
+  for (var i = 0; i < urls.length; i++) {
+    let url = urls[i];
+    let newUrl = '';
+    messageContent = messageContent.replace(url, '');
+    newUrl = url.replace('mobile.', '');
+    newUrl = newUrl.replace(/\?.*\b/g, '');
+    newUrl = newUrl.replace('/twitter', `/${ config.prefix }twitter`);
+    newUrls.push(newUrl);
+  }
+
+}
+
+function messageHasEmbed(embeds) {
+  for (var i = 0; i < embeds.length; i++) {
+    let item = embeds[i];
+    if (item.image || item.video) {
+      return true;
+    }
+  }
+  return false;
+}
+
+client.on('messageDelete', (message) => {
+  
 })
 
 client.login(loginToken);
